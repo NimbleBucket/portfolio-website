989585707d2da4b8abc32a0ef854fa63e3de8252
Unknown <nathanclark38@gmail.com>
2017-03-03 15:38:37 -0600

    Adding github pages support

--- src/router.js
+++ src/router.js
@@ -2,13 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/App';
 import TimerSetContainer from './components/containers/timerset-container';
-import {Router, Route, browserHistory, IndexRoute} from 'react-router';
+import {Router, Route, hashHistory, IndexRoute} from 'react-router';
 import Whoops404 from './components/views/Whoops404';
 import navbar from './navbar.js';
 
 export default (
 
-  <Router history={browserHistory}>
+  <Router history={hashHistory}>
     <Route path='/' component={navbar}>
       <Route path='Timer' component={TimerSetContainer} />
     </Route>
